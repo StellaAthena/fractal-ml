6437c90299df48127348c3a32e152629b5784b5d
sven <sven@endgame.com>
2020-06-04 13:00:41 -0700

    hash based box dimension done

--- src/lib.rs
+++ src/lib.rs
@@ -6,44 +6,73 @@ use numpy::{IntoPyArray, PyArray2, PyArray3};
 use rand::distributions::{Distribution, Uniform};
 use rand::thread_rng;
 
-#[pymodule]
-fn fractal_ml(py: Python, m: &PyModule) -> PyResult<()> {
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
 
+fn calculate_hash<T: Hash>(t: &[f64]) -> u64 {
+    
+    t.hash(&mut s);
+    s.finish()
+}
+
+#[pymodule]
+fn fractal_ml(_py: Python, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "generate_direction_fractal")]
     pub fn generate_direction_fractal(
         num_samples: u64,
         step_scale: f64,
         directions: &PyArray3<f64>,
     ) -> PyResult<Py<PyArray2<f64>>> {
-        let len: usize = directions.shape()[0];
-        let num_directions: usize = directions.shape()[1];
-        let dim: usize = directions.shape()[2];
+        let len = directions.shape()[0];
+        let num_directions = directions.shape()[1];
+        let dim = directions.shape()[2];
 
         let mut rng = thread_rng();
         let direction_dist = Uniform::from(0..num_directions);
 
-        let mut output: Vec<f64> = vec![0.0; num_samples as usize * dim];
+        let mut output: Array2<f64> = Array2::zeros(Ix2(num_samples as usize, dim));
 
         for j in 0..(num_samples as usize) {
-            let output_slice = &mut output[j * dim..(j + 1) * dim];
+            let mut output_slice: ArrayViewMut1<f64> = output.slice_mut(s![j, ..]);
             for i in 0..len {
                 let k: usize = direction_dist.sample(&mut rng);
-                output_slice
-                    .iter_mut()
-                    .zip(
-                        directions
-                            .as_array()
-                            .slice(s![i, k, ..])
-                            .as_slice()
-                            .unwrap(),
-                    )
-                    .for_each(|(a, b)| *a += b * step_scale.powi(i as i32));
+                output_slice += &directions.as_array().slice(s![len - i - 1, k, ..]);
+                output_slice *= step_scale;
             }
         }
-        let py_output = Array2::from_shape_vec((num_samples as usize, dim), output).unwrap();
         let gil = GILGuard::acquire();
         let py = gil.python();
-        Ok(py_output.into_pyarray(py).to_owned())
+        Ok(output.into_pyarray(py).to_owned())
+    }
+
+    #[pyfn(m, "approx_box_counting")]
+    pub fn approx_box_counting(
+        scaling_factor: f64,
+        max_scale: i32,
+        min_scale: i32,
+        dataset: &PyArray2<f64>,
+    ) -> Vec<u64> {
+        let len = dataset.shape()[0];
+        let dim = dataset.shape()[1];
+
+        let mut output = Vec::with_capacity(len);
+
+        for j in 0..((max_scale - min_scale) as usize) {
+            let hashes: HashSet<u64> = HashSet::new();
+            let scale: f64 = scaling_factor.powi(j as i32 - min_scale);
+            for i in 0..len {
+                let mut s = DefaultHasher::new();
+                output_slice: Vec<u64> = directions
+                    .as_array()
+                    .slice(s![len, ..])
+                    .for_each(|x| (x * scale).floor().to_bits().hash(&mut s));
+                hashes.insert(s.finish());
+            }
+            output.push(hashes.len() as u64);
+        }
+        
+        output
     }
 
     Ok(())
